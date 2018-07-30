2802fb74576fb84ede0c03c179226268948753d1
kieransp11 <kieransparrott@gmail.com>
2018-07-30 12:53:42 +0100

    Update msfinstall.sh
    Altered switch user mechanism

--- msfinstall.sh
+++ msfinstall.sh
@@ -3,10 +3,8 @@
 apt-get install gpgv2 autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl postgresql postgresql-contrib wget xsel zlib1g zlib1g-dev
 
 # Configure postgres db
-su postgres
-createuser msfuser -S -R
-createdb msfdb -O msfuser
-exit
+sudo -u postgres createuser msfuser -S -R
+sudo -u postgres createdb msfdb -O msfuser
 update-rc.d postgresql enable
 
 # Install and configure rvm
