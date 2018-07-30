c2ba1d11529bc65d33ccba41984ec790f5869b3c
kieransp11 <kieransparrott@gmail.com>
2018-07-30 14:47:30 +0100

    Update msfinstall.sh
    rvm fix

--- msfinstall.sh
+++ msfinstall.sh
@@ -10,8 +10,8 @@ update-rc.d postgresql enable
 # Install and configure rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-rvm --install .ruby-version
 source ~/.rvm/scripts/rvm
+rvm --install .ruby-version
 
 # Download and install msf
 git clone https://github.com/rapid7/metasploit-framework.git
