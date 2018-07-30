fec2331d284717f25d6e3e917ba9bfe129ce8330
kieransp11 <kieransparrott@gmail.com>
2018-07-30 14:07:41 +0100

    Update msfinstall.sh
    sudo fix

--- msfinstall.sh
+++ msfinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Metasploit dependencies
-apt-get install gpgv2 autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl postgresql postgresql-contrib wget xsel zlib1g zlib1g-dev
+sudo apt-get install gpgv2 autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl postgresql postgresql-contrib wget xsel zlib1g zlib1g-dev
 
 # Configure postgres db
 sudo -u postgres createuser msfuser -S -R
