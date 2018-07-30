aab13bcf62e072c8d35617397b81de1c09184daa
kieransp11 <kieransparrott@gmail.com>
2018-07-30 16:24:25 +0100

    Update msfinstall.sh
    bash supports source

--- msfinstall.sh
+++ msfinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Metasploit dependencies
 sudo apt-get install gpgv2 autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl postgresql postgresql-contrib wget xsel zlib1g zlib1g-dev
 
